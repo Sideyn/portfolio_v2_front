01d295de52970c79fe85bebb6a4400f791a814f2
Sidney <sidney.croset@hotmail.com>
2022-03-29 14:38:47 +0200

    footer + responsive ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 
 function Home() {
   return (
     <div className="home">
       <Header />
+      <Footer />
     </div>
   );
 }
